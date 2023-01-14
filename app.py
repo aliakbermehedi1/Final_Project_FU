8bfc2e9861e88b9183dbfbdc564ed18e08fc2ef0
Ali Akber Mehedi <78915374+aliakbermehedi1@users.noreply.github.com>
2023-01-14 10:34:00 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+
+    return " ".join(y)
+
+tfidf = pickle.
+st.title("Email/SMS Spam Classifier")
+
+a("Enter the message")
+
+if st.button('Predict'):
+
+    vector_input[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
